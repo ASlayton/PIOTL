7a70b2686f6074fa25dde3625110102fb4866344
Amanda Slayton <alslayton86@gmail.com>
2019-01-26 15:40:17 -0600

    grocery controller

--- PIOTL-Backend/PIOTL/Controllers/GroceryController.cs
+++ PIOTL-Backend/PIOTL/Controllers/GroceryController.cs
@@ -2,14 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PIOTL.DataAccess;
+using PIOTL.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace PIOTL.Controllers
+namespace BangazonInc.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class GroceryController : ControllerBase
     {
+        DatabaseInterface _db;
+        GroceryStorage _Grocery;
+
+        public GroceryController(DatabaseInterface db)
+        {
+            _db = db;
+            _Grocery = new GroceryStorage(db);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allGrocerys = _Grocery.GetGrocery();
+            return Ok(allGrocerys);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGroceryById(int id)
+        {
+            var singleGrocery = _Grocery.GetGroceryById(id);
+            return Ok(singleGrocery);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteById(int id)
+        {
+            var Grocery = _Grocery.GetGroceryById(id);
+
+            if (Grocery == null)
+            {
+                return NotFound();
+            }
+
+            var success = _Grocery.DeleteById(id);
+
+            if (success)
+            {
+                return Ok();
+            }
+            return BadRequest(new { Message = "Delete was unsuccessful" });
+        }
+
+        [HttpPut("Grocery")]
+        public IActionResult UpdateGrocery(Grocery Grocery)
+        {
+            var Grocerys = _Grocery.UpdateGrocery(Grocery);
+            return Ok();
+        }
+
+        [HttpPost("Grocery")]
+        public async Task<ActionResult<Grocery>> PostGrocery(GroceryWithEmployeeId Grocery)
+        {
+            return Grocery.EmployeeId is null
+                ? Ok(await _Grocery.PostGrocery(Grocery))
+                : Ok(await _Grocery.PostGroceryAndAssignToEmployee(Grocery));
+        }
     }
 }
