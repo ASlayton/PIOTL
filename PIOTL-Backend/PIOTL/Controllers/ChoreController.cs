441dc8a0fcba7bb6b3e06c57a0fffd5d9d83b699
Amanda Slayton <alslayton86@gmail.com>
2019-01-26 15:36:06 -0600

    event controller

--- PIOTL-Backend/PIOTL/Controllers/ChoreController.cs
+++ PIOTL-Backend/PIOTL/Controllers/ChoreController.cs
@@ -2,14 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PIOTL.DataAccess;
+using PIOTL.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace PIOTL.Controllers
+namespace BangazonInc.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ChoreController : ControllerBase
     {
+        DatabaseInterface _db;
+        ChoreStorage _Chore;
+
+        public ChoreController(DatabaseInterface db)
+        {
+            _db = db;
+            _Chore = new ChoreStorage(db);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allChores = _Chore.GetChore();
+            return Ok(allChores);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetChoreById(int id)
+        {
+            var singleChore = _Chore.GetChoreById(id);
+            return Ok(singleChore);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteById(int id)
+        {
+            var Chore = _Chore.GetChoreById(id);
+
+            if (Chore == null)
+            {
+                return NotFound();
+            }
+
+            var success = _Chore.DeleteById(id);
+
+            if (success)
+            {
+                return Ok();
+            }
+            return BadRequest(new { Message = "Delete was unsuccessful" });
+        }
+
+        [HttpPut("Chore")]
+        public IActionResult UpdateChore(Chore Chore)
+        {
+            var Chores = _Chore.UpdateChore(Chore);
+            return Ok();
+        }
+
+        [HttpPost("Chore")]
+        public async Task<ActionResult<Chore>> PostChore(ChoreWithEmployeeId Chore)
+        {
+            return Chore.EmployeeId is null
+                ? Ok(await _Chore.PostChore(Chore))
+                : Ok(await _Chore.PostChoreAndAssignToEmployee(Chore));
+        }
     }
 }
--- PIOTL-Backend/PIOTL/Controllers/EventController.cs
+++ PIOTL-Backend/PIOTL/Controllers/EventController.cs
@@ -2,14 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PIOTL.DataAccess;
+using PIOTL.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace PIOTL.Controllers
+namespace BangazonInc.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class EventController : ControllerBase
     {
+        DatabaseInterface _db;
+        EventStorage _Event;
+
+        public EventController(DatabaseInterface db)
+        {
+            _db = db;
+            _Event = new EventStorage(db);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allEvents = _Event.GetEvent();
+            return Ok(allEvents);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEventById(int id)
+        {
+            var singleEvent = _Event.GetEventById(id);
+            return Ok(singleEvent);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteById(int id)
+        {
+            var Event = _Event.GetEventById(id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+
+            var success = _Event.DeleteById(id);
+
+            if (success)
+            {
+                return Ok();
+            }
+            return BadRequest(new { Message = "Delete was unsuccessful" });
+        }
+
+        [HttpPut("Event")]
+        public IActionResult UpdateEvent(Event Event)
+        {
+            var Events = _Event.UpdateEvent(Event);
+            return Ok();
+        }
+
+        [HttpPost("Event")]
+        public async Task<ActionResult<Event>> PostEvent(EventWithEmployeeId Event)
+        {
+            return Event.EmployeeId is null
+                ? Ok(await _Event.PostEvent(Event))
+                : Ok(await _Event.PostEventAndAssignToEmployee(Event));
+        }
     }
 }
