c3bfd948154b4efc3f77c79214d38562da09fcd1
Amanda Slayton <alslayton86@gmail.com>
2019-01-29 20:34:53 -0600

    wip

--- PIOTL-Frontend/src/components/Header/Header.js
+++ PIOTL-Frontend/src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import authRequests from  '../../firebaseRequests/auth';
-import './Navbar.css';
+import './Header.css';
 
 class Navbar extends React.Component {
   render () {
