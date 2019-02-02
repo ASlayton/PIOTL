2eb072fb1dca5991d492efbe4b75889139983acb
Amanda Slayton <alslayton86@gmail.com>
2019-02-02 11:48:33 -0600

    able to get user by firebase id and retrieve info based on that id

--- PIOTL-Frontend/src/components/Home/Home.js
+++ PIOTL-Frontend/src/components/Home/Home.js
@@ -1,18 +1,31 @@
 import './Home.css';
 import React from 'react';
 import Memo from '../Memo/Memo';
+import api from '../../api-access/api';
+import auth from '../../firebaseRequests/auth';
 
-// IF NOT SIGNED IN, USER IS DIRECTED HERE
+// IF SIGNED IN, USER IS DIRECTED HERE
 class Home extends React.Component {
   state = {
     user: {},
+  };
+  componentDidMount () {
+    const id = auth.getUid();
+    api.apiGet('User/' + id)
+      .then(res => {
+        const data = res.data;
+        this.setState({user: data});
+      })
+      .catch((err) => {
+        console.error('Error with user get request', err);
+      });
   };
 
   render () {
     return (
       <div>
         <div>
-          <Memo />
+          <Memo user={this.state.user}/>
         </div>
       </div>
     );
