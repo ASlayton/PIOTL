f9ff18b71baa6f4c02a3f74a031e57819fc54ec8
Amanda Slayton <alslayton86@gmail.com>
2019-02-09 13:30:42 -0600

    register is more robust

--- PIOTL-Frontend/src/components/Register/Register.js
+++ PIOTL-Frontend/src/components/Register/Register.js
@@ -13,6 +13,10 @@ class Register extends React.Component {
       firstName: '',
       lastName: '',
     },
+    haveFamily: false,
+    familyName: '',
+    familyId: 1,
+    isChild: true
   }
 
   onInputChange = (e) => {
@@ -35,17 +39,65 @@ class Register extends React.Component {
           firstName: user.firstName,
           lastName: user.lastName,
           firebaseId: user.firebaseId,
+          adult: !this.state.isChild,
+          familyId: this.state.familyId,
+          earned: 0
         };
-        api.apiPost('user/register', newCustomer).then(response => {
-          this.props.signin(response.data);
-          this.props.history.push('/');
-        });
+        api.apiPost('user/register', newCustomer)
+          .then(response => {
+            console.log('I have posted: ', response);
+            this.props.signin(response.data);
+            this.props.history.push('/');
+          })
+          .catch((err) => {
+            console.log('I am not posting your user', err);
+          });
       })
       .catch(err => {
         this.setState({isError: true, error: err.message});
       });
 
   }
+  changeHandler1 = (e) => {
+    if (e.target.value) {
+      this.setState({haveFamily: true});
+    } else {
+      this.setState({haveFamily: false});
+    };
+  }
+
+  changeHandler2 = (e) => {
+    if (e.target.value) {
+      this.setState({haveFamily: false});
+    } else {
+      this.setState({haveFamily: false});
+    };
+  }
+
+  isChildHandler1 = (e) => {
+    if (e.target.value) {
+      this.setState({isChild: true});
+    }
+  };
+
+  isChildHandler2 = (e) => {
+    if (e.target.value) {
+      this.setState({isChild: false});
+    }
+  };
+
+  haveFamilyNameHandler = (e) => {
+    const familyName = e.target.value;
+    this.setState({familyId: 0});
+    this.setState({familyName: familyName});
+  };
+
+  haveFamilyIdHandler = (e) => {
+    const familyId = e.target.value;
+    this.setState({familyName: ''});
+    this.setState({familyId: familyId});
+  };
+
   render () {
     return (
       <div className='LoginForm'>
@@ -70,12 +122,34 @@ class Register extends React.Component {
                   <label htmlFor="lastName">Last Name</label>
                   <input type="text" id="lastName" className="form-control" placeholder="Smith" value={this.state.user.lastName} onChange={this.onInputChange} />
                 </div>
+                <div className="form-group col">
+                  <p>Is your family already registered?</p>
+                  <input type="radio" id="havefamily-yes" name="family" onChange={this.changeHandler1}/>
+                  <label htmlFor="haveFamily_yes">Yes</label>
+
+                  <input type="radio" id="havefamily-no" name="family" onChange={this.changeHandler2}/>
+                  <label htmlFor="haveFamily_no">No</label>
+                  {
+                    this.state.haveFamily ? (
+                      <input type="text" placeholder="Insert Family Id Number" onChange={this.haveFamilyNameHandler} />
+                    ) : <input type="text" placeholder="Enter your family name"  onChange={this.haveFamilyIdHandler} />
+                  }
+                </div>
               </div>
               {
                 this.state.isError ? (
                   <div className="alert alert-danger">{this.state.error}</div>
                 ) : null
               }
+
+              <div>
+                <p>Are you over 13?</p>
+                <input type="radio" name="admin" id="isChild" onChange={this.isChildHandler1}/>
+                <label htmlFor="isChild">Yes</label>
+                <input type="radio" name="admin" id="isNotChild" onChange={this.isChildHandler2}
+                />
+                <label htmlFor="isNotChild">No</label>
+              </div>
               <button type="submit" className="btn btn-primary">Register</button>
               <small><Link to="/login">Already registered?</Link></small>
             </form>
